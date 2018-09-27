59667eb754015a470a4b5223a15f872be32772ac
olyayandukova olyayandukova <you@example.com>
2018-09-27 10:48:04 +0400

    Laba_12

---
+++ AbstractBar/AbstractBarService/Interfaces/IReportService.cs
@@ -0,0 +1,23 @@
+﻿using AbstractBarService.BindingModels;
+using AbstractBarService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractBarService.Interfaces
+{
+    public interface IReportService
+    {
+        void SaveCoctailPrice(ReportBindingModel model);
+
+        List<StoragesLoadViewModel> GetStoragesLoad();
+
+        void SaveStoragesLoad(ReportBindingModel model);
+
+        List<CustomerRequestsModel> GetCustomerRequests(ReportBindingModel model);
+
+        void SaveCustomerRequests(ReportBindingModel model);
+    }
+}
